c220ae23a2d65b12c83657458aec2976eb014833
Ahmed Badry <ra6514201@gmail.com>
2024-03-20 22:46:04 +0200

    updata-img-2

--- src/pages/services/Services.jsx
+++ src/pages/services/Services.jsx
@@ -3,13 +3,14 @@ import Header from '../../components/Header'
 import Hero2 from "../../components/Hero2";
 import ServiceCard2 from '../../components/ServiceCard2';
 import Footer from '../../components/Footer';
+import SAppointment from '/src/assets/SAppointment.png'
 import './services.css';
 function Services() {
   return (
     <div>
       <Header />
       <Hero2 title="Our Services" image="/src/assets/Medical-Services1.png" />
-      <ServiceCard2 image="/public/SAppointment.png" title="Up Rumors" description="Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys." btnText="Up Rumors" link="/Up Rumors" />
+      <ServiceCard2 image={SAppointment} title="Up Rumors" description="Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys." btnText="Up Rumors" link="/Up Rumors" />
       <ServiceCard2 image="/src/assets/s2.png" title="Our Doctor" description="Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys." btnText="Find a doctor" link="/find-a-doctor" />
       <ServiceCard2 image="/src/assets/s1.png" title="Appointments" description="Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys.Explore the experiences of other individuals facing similar journeys." btnText="Appointments" link="/PatientVisitor" />
       <Footer />
